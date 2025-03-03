79ee935c987ab9a6751f9d3e7ae98e93bde7a36f
Lencho123 <lincholechisa42@gmail.com>
2025-03-03 10:04:25 +0300

    deployment of my project

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/login-sinUp/",
+  base: "/react-project-fully-deployed-Bankdash",
   // base: "/BankDash-in-reactJS",
 });
